6e18a24c2e32212933e89c0ba3ae7de47df91130
Alexander Hörl <alexander.horl2@live.de>
2022-08-19 14:05:28 +0200

    fixed raw2temp func crashing in next_frame; added frame_at and get_metadata methods; count_frames returns number of frames; added examples; formatted using Black formatter; added VSCode and macOS to gitignore

---
+++ examples/export_all_frames.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+from pathlib import Path
+from sys import argv, path
+
+path.append("../csq")
+from csq import CSQReader
+
+file_path = argv[1]
+output = argv[2]
+filename = Path(path).name
+reader = CSQReader(file_path)
+
+
+def export_thermal_image(frame, name: str):
+    filen_name = output + name + ".png"
+    hot = plt.cm.hot
+    norm = plt.Normalize(vmin=frame.min(), vmax=frame.max())
+    image = hot(norm(frame))
+    plt.imsave(filen_name, image)
+
+
+i = 0
+reader._populate_list()
+for frame in reader.frames():
+    export_thermal_image(frame, name=filename + "_" + str(i))
+    i += 1
---
+++ examples/plot_frame.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import matplotlib.pyplot as plt
+from sys import argv, path
+
+path.append("../csq")
+from csq import CSQReader
+
+
+file_path = argv[1]
+reader = CSQReader(file_path)
+
+
+def plot_frame(frame):
+
+    ax = plt.gca()
+    plt.axis("off")
+    im = plt.imshow(frame, cmap="inferno")
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    cbar = plt.colorbar(im, cax=cax)
+    cbar.ax.set_ylabel("Temperature ($^{\circ}$C)", fontsize=14)
+    plt.show()
+
+
+frame = reader.frame_at(0)
+plot_frame(frame)
