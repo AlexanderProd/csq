30e79c4938e7c52558fafa199d4002a0eb6cc21f
Alexander Hörl <alexander.horl2@live.de>
2023-02-04 17:37:06 +0100

    using render_video from export_as_video

--- examples/export_as_video_all_filters.py
+++ examples/export_as_video_all_filters.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python3
+from __future__ import annotations
 
-import matplotlib.pyplot as plt
 from pathlib import Path
 from sys import argv, path
 import os
-import numpy as np
+from matplotlib.colors import LinearSegmentedColormap
 
 path.append("../csq")
-from csq import CSQReader
-from colormaps import green_hot, cold_green, red_hot, cold_red, ironbow
-import cv2
+from colormaps import green_hot, cold_green, red_hot, cold_red, ironbow, hottest
+from export_as_video import v1 as render_video
 
 
 file_path = argv[1]
 output = argv[2]
 input_filename = Path(file_path).name
-reader = CSQReader(file_path)
 
-
-filters = [
+filters: list[LinearSegmentedColormap | str] = [
+    green_hot,
+    cold_green,
+    red_hot,
+    cold_red,
+    ironbow,
+    hottest,
     "hot",
     "cool",
     "jet",
@@ -27,47 +30,22 @@ filters = [
 ]
 
 
-def export_thermal_image(frame, filter=plt.cm.hot):
-    norm = plt.Normalize(vmin=frame.min(), vmax=frame.max())
-
-    colormap = filter
-    if type(filter) == str:
-        colormap = plt.colormaps[filter]
-
-    image = colormap(norm(frame))
-    return image
-
-
-def render_video(output_filename: str, filter):
-    video_writer = None
-
-    reader._populate_list()
-    for frame in reader.frames():
-        img = export_thermal_image(frame, filter)
-
-        if video_writer is None:
-            height, width, _ = img.shape
-            fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-            video_writer = cv2.VideoWriter(
-                output_filename, fourcc, 30.0, (int(width), int(height))
-            )
-
-        img = img[:, :, :3]  # remove alpha channel
-        img = img[..., ::-1]  # flip channels (RGB to BGR)
-
-        img = np.uint8(img * 255)
-        video_writer.write(img)
-
-
 def main():
     if not os.path.isdir(output):
         print("The provided output path is not a directory.")
         os.exit(1)
 
     for filter in filters:
-        output_filename = output + input_filename + "_" + filter + ".mp4"
+        filtername: str = ""
+        if type(filter) == str:
+            filtername = filter
+
+        if type(filter) == LinearSegmentedColormap:
+            filtername = filter.name
+
+        output_filename = output + input_filename + "_" + filtername + ".mp4"
         print(output_filename)
-        render_video(output_filename, filter)
+        render_video(input_file=file_path, output_video=output_filename, filter=filter)
 
 
 main()
